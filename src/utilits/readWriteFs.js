2f25f22a2bde8ec58fc4a9b8383a55cc777557b7
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-08-30 17:32:10 -0300

    feat: corrige função join.

--- src/utilits/readWriteFs.js
+++ src/utilits/readWriteFs.js
@@ -5,7 +5,7 @@ const fileName = '../src/talker.json';
 
 const readTakerFile = async () => {
   try {
-    const arrayTalker = await fs.readFile(join(__dirname, fileName, 'utf-8'));
+    const arrayTalker = await fs.readFile(join(__dirname, fileName), 'utf-8');
     const TalkerList = JSON.parse(arrayTalker);
     return TalkerList; 
   } catch (err) {
