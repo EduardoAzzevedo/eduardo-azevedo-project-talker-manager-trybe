b53f57d6f5cab3a3b642e80b30c2a30d36521afa
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-09-05 12:33:56 -0300

    feat: cria funções de validação: token, name, age, talk, watchedAt.

---
+++ src/middlewaresTalker/watchedAtValidation.js
@@ -0,0 +1,13 @@
+const watchedValidation = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const dataForm = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  if (!watchedAt || watchedAt === '') {
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  if (!dataForm.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+module.exports = watchedValidation;
--- src/routerTalker/routerPostTalker.js
+++ src/routerTalker/routerPostTalker.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const { postNewTalker } = require('../utilits/readWriteFs');
+const tokenValidation = require('../middlewaresTalker/tokenValidation');
+const nameValidation = require('../middlewaresTalker/nameValidation');
+const ageValidation = require('../middlewaresTalker/ageValidation');
+const talkValidation = require('../middlewaresTalker/talkValidation');
+const watchedValidation = require('../middlewaresTalker/watchedAtValidation');
 
 const routerPost = express.Router();
 
-routerPost.post('/talker', async (req, res) => {
+routerPost.post('/talker',
+tokenValidation, nameValidation,
+ageValidation, talkValidation,
+watchedValidation, async (req, res) => {
   const talkerPost = req.body;
   const insertTalker = await postNewTalker(talkerPost);
   console.log('incluido', insertTalker);
